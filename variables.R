de673fd272bc7781912cd134939875db0e5bf029
Himakar123 <103348571+Himakar123@users.noreply.github.com>
2023-03-28 22:52:06 +0530

    Add files via upload
    in this, we learn how to assign values to variables and how to remove or delete them.

---
+++ variables.R
@@ -0,0 +1,18 @@
+x<- 2
+# this is the creation of  a variable ,the operater used is used to assign the value.
+x<- 3
+x
+y<-c(1,2,3,4,5)
+z<- 1:10
+x<- y <-1:10
+x+y
+z<- x+y
+z1<- x*y
+# it is a case sensitive language.
+ls()
+rm (x)
+# the above command helps to delete the varisble
+remove(z1)
+rm(list=ls)
+# helps to delete all the variables
+rm(list=ls())
